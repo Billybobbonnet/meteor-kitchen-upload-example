3b49271431b703437bacd4f11376f2dd5982ca8d
Billybobbonnet <boitalettre9@gmail.com>
2015-05-03 18:30:59 +0200

    first commit

---
+++ client/views/router.js
@@ -0,0 +1,132 @@
+Router.configure({
+	templateNameConverter: "upperCamelCase",
+	routeControllerNameConverter: "upperCamelCase",
+	layoutTemplate: "layout",
+	notFoundTemplate: "notFound",
+	loadingTemplate: "loading"
+});
+
+var publicRoutes = ["home_public", "login", "register", "forgot_password", "reset_password"];
+var privateRoutes = ["home_private", "user_settings", "user_settings.profile", "user_settings.change_pass", "logout"];
+var zonelessRoutes = [];
+
+var roleMap = [
+	
+];
+
+this.firstGrantedRoute = function() {
+	var grantedRoute = "";
+	_.every(privateRoutes, function(route) {
+		if(routeGranted(route)) {
+			grantedRoute = route;
+			return false;
+		}
+		return true;
+	});
+
+	if(grantedRoute == "") {
+		if(routeGranted("home_private")) {
+			return "home_private";				
+		} else {
+			return "home_public";
+		}
+	}
+
+	return grantedRoute;
+}
+
+// this function returns true if user is in role allowed to access given route
+this.routeGranted = function(routeName) {
+	if(!routeName) {
+		// route without name - enable access (?)
+		return true;
+	}
+
+	if(!roleMap || roleMap.length === 0) {
+		// this app don't have role map - enable access
+		return true;
+	}
+
+	var roleMapItem = _.find(roleMap, function(roleItem) { return roleItem.route == routeName; });
+	if(!roleMapItem) {
+		// page is not restricted
+		return true;
+	}
+
+	if(!Meteor.user() || !Meteor.user().roles) {
+		// user is not logged in
+		return false;
+	}
+
+	// this page is restricted to some role(s), check if user is in one of allowedRoles
+	var allowedRoles = roleMapItem.roles;
+	var granted = _.intersection(allowedRoles, Meteor.user().roles);
+	if(!granted || granted.length === 0) {
+		return false;
+	}
+
+	return true;
+};
+
+Router.ensureLogged = function() {
+	if(Meteor.userId() && (!Meteor.user() || !Meteor.user().roles)) {
+		return;
+	}
+
+	if(!Meteor.userId()) {
+		// user is not logged in - redirect to public home
+		this.redirect("home_public");
+	} else {
+		// user is logged in - check role
+		if(!routeGranted(this.route.getName())) {
+			// user is not in allowedRoles - redirect to private home
+			var redirectRoute = firstGrantedRoute();
+			this.redirect(redirectRoute);
+		} else {
+			this.next();
+		}
+	}
+};
+
+Router.ensureNotLogged = function() {
+	if(Meteor.userId() && (!Meteor.user() || !Meteor.user().roles)) {
+		return;
+	}
+
+	if(Meteor.userId()) {
+		var redirectRoute = firstGrantedRoute();
+		this.redirect(redirectRoute);
+	}
+	else {
+		this.next();
+	}
+};
+
+Meteor.subscribe("current_user_data");
+
+Router.onBeforeAction(function() {
+	// loading indicator here
+	if(!this.ready()) {
+		$("body").addClass("wait");
+	} else {
+		$("body").removeClass("wait");
+		this.next();
+	}
+});
+
+Router.onBeforeAction(Router.ensureNotLogged, {only: publicRoutes});
+Router.onBeforeAction(Router.ensureLogged, {only: privateRoutes});
+
+Router.map(function () {
+	
+	this.route("home_public", {path: "/", controller: "HomePublicController"});
+	this.route("login", {path: "/login", controller: "LoginController"});
+	this.route("register", {path: "/register", controller: "RegisterController"});
+	this.route("forgot_password", {path: "/forgot_password", controller: "ForgotPasswordController"});
+	this.route("reset_password", {path: "/reset_password/:resetPasswordToken", controller: "ResetPasswordController"});
+	this.route("home_private", {path: "/home_private", controller: "HomePrivateController"});
+	this.route("user_settings", {path: "/user_settings", controller: "UserSettingsController"});
+	this.route("user_settings.profile", {path: "/user_settings/profile", controller: "UserSettingsProfileController"});
+	this.route("user_settings.change_pass", {path: "/user_settings/change_pass", controller: "UserSettingsChangePassController"});
+	this.route("logout", {path: "/logout", controller: "LogoutController"});
+});
---
+++ server/server.js
@@ -0,0 +1,120 @@
+Meteor.startup(function() {
+	// read environment variables from Meteor.settings
+	if(Meteor.settings && Meteor.settings.env && _.isObject(Meteor.settings.env)) {
+		for(var variableName in Meteor.settings.env) {
+			process.env[variableName] = Meteor.settings.env[variableName];
+		}
+	}
+
+	
+});
+
+Meteor.methods({
+	"createUserAccount": function(options) {
+		if(!Users.isAdmin(Meteor.userId())) {
+			throw new Meteor.Error(403, "Access denied.");
+		}
+
+		var userOptions = {};		
+		if(options.username) userOptions.username = options.username;
+		if(options.email) userOptions.email = options.email;
+		if(options.password) userOptions.password = options.password;
+		if(options.profile) userOptions.profile = options.profile;
+		if(options.profile && options.profile.email) userOptions.email = options.profile.email;
+
+		Accounts.createUser(userOptions);
+	},
+	"updateUserAccount": function(userId, options) {
+		// only admin or users own profile
+		if(!(Users.isAdmin(Meteor.userId()) || userId == Meteor.userId())) {
+			throw new Meteor.Error(403, "Access denied.");
+		}
+
+		// non-admin user can change only profile
+		if(!Users.isAdmin(Meteor.userId())) {
+			var keys = Object.keys(options);
+			if(keys.length !== 1 || !options.profile) {
+				throw new Meteor.Error(403, "Access denied.");
+			}
+		}
+
+		var userOptions = {};		
+		if(options.username) userOptions.username = options.username;
+		if(options.email) userOptions.email = options.email;
+		if(options.password) userOptions.password = options.password;
+		if(options.profile) userOptions.profile = options.profile;
+
+		if(options.profile && options.profile.email) userOptions.email = options.profile.email;
+		if(options.roles) userOptions.roles = options.roles;
+
+		if(userOptions.email) {
+			var email = userOptions.email;
+			delete userOptions.email;
+			userOptions.emails = [{ address: email }];
+		}
+
+		var password = "";
+		if(userOptions.password) {
+			password = userOptions.password;
+			delete userOptions.password;
+		}
+
+		if(userOptions) {
+			Users.update(userId, { $set: userOptions });
+		}
+
+		if(password) {
+			Accounts.setPassword(userId, password);
+		}
+	},
+
+	"sendMail": function(options) {
+		this.unblock();
+
+		Email.send(options);
+	}
+});
+
+Accounts.onCreateUser(function (options, user) {
+	user.roles = [];
+
+	if(options.profile) {
+		user.profile = options.profile;
+	}
+
+	
+
+	return user;
+});
+
+Accounts.validateLoginAttempt(function(info) {
+
+	// reject users with role "blocked"
+	if(info.user && Users.isInRole(info.user._id, "blocked")) {
+		throw new Meteor.Error(403, "Your account is blocked.");
+	}
+
+	return true;
+});
+
+
+Users.before.insert(function(userId, doc) {
+	if(doc.emails && doc.emails[0] && doc.emails[0].address) {
+		doc.profile = doc.profile || {};
+		doc.profile.email = doc.emails[0].address;
+	}
+});
+
+Users.before.update(function(userId, doc, fieldNames, modifier, options) {
+	if(modifier.$set && modifier.$set.emails && modifier.$set.emails.length && modifier.$set.emails[0].address) {
+		modifier.$set.profile.email = modifier.$set.emails[0].address;
+	}
+});
+
+Accounts.onLogin(function (info) {
+	
+});
+
+Accounts.urls.resetPassword = function (token) {
+	return Meteor.absoluteUrl('reset_password/' + token);
+};
